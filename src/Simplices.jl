f396d802063e547bd02c44a87bb9b5e9efebc7ed
kahaaga <kahaaga@gmail.com>
2018-03-24 03:40:07 +0100

    Added simplex functions.

--- src/Simplices.jl
+++ src/Simplices.jl
@@ -4,36 +4,36 @@ include("barycentric-coordinates.jl")
 include("Binary.jl")
 include("Circumsphere.jl")
 include("complementary.jl")
+include("centroids_radii.jl")
+include("embed.jl")
 include("geometry.jl")
 include("heaviside.jl")
 include("intersection-of-boundaries.jl")
 include("intersection-of-boundaries-loop.jl")
-include("some-vertex-in-circumsphere.jl")
-include("sharing-a-face.jl")
 include("init_functions.jl")
+include("inside_simplex.jl")
 include("NullSpace.jl")
 include("polytope-generating-vertices.jl")
 include("QR.jl")
 include("SimplexChecks.jl")
 include("shared-vertices.jl")
+include("sharing-a-face.jl")
+include("some-vertex-in-circumsphere.jl")
 include("simplexoperations.jl")
 include("simplexintersect.jl")
-include("TriangulationPolytopeFaces.jl")
-include("TriangulationNonSimplicialFaces.jl")
-include("Update.jl")
-include("volume-computation.jl")
-include("centroids_radii.jl")
-include("embed.jl")
 include("simplex_split.jl")
 include("simplex_subdivision.jl")
 include("simplex_subdivision_single.jl")
 include("simplexvolumes.jl")
-
+include("TriangulationPolytopeFaces.jl")
+include("TriangulationNonSimplicialFaces.jl")
+include("Update.jl")
+include("volume-computation.jl")
 
 export BarycentricCoordinates,
         centroid,
         Circumsphere,
-        childsimplex,
+        childsimplex, childpoint, childpoints,
         Delaunay,
         heaviside0,
         intersecting_simplices,
@@ -43,6 +43,7 @@ export BarycentricCoordinates,
         issingular,
         nontrivially_intersecting_simplices,
         nontrivial_intersection,
+        outsidepoint,
         orientation,
         PolytopeGeneratingVertices,
         radius,
--- src/inside_simplex.jl
+++ src/inside_simplex.jl
@@ -11,9 +11,11 @@ function childsimplex(parentsimplex::Array{Float64, 2}, dim::Int)
     (normalised_colsums .* rs) * parentsimplex
 end
 
-
-function childpoint(parentsimplex::Array{Float64, 2})
-    dim = size(parentsimplex, 2)
+"""
+Create a point that lies inside `parent_simplex`
+"""
+function childpoint(parent_simplex::Array{Float64, 2})
+    dim = size(parent_simplex, 2)
     # Random linear combination coefficients
     R = rand(1, dim + 1)
 
@@ -25,6 +27,25 @@ function childpoint(parentsimplex::Array{Float64, 2})
 end
 
 
+"""
+Create multiple (default = 100) points that lies inside `parent_simplex`.
+"""
+function childpoints(parent_simplex::Array{Float64, 2}; n::Int = 100)
+    dim = size(parent_simplex, 2)
+    # Random linear combination coefficients
+    R = rand(n, dim + 1)
+
+    # Normalise the coefficients so that they sum to one. We can then create the new point
+    # as a convex linear combination of the vertices of the parent simplex.
+    normalised_coeffs = (1 ./ sum(R, 2)) .* R
+
+    normalised_coeffs * parentsimplex
+end
+
+
+"""
+Create a point that lies inside `parent_simplex`
+"""
 function outsidepoint(parentsimplex::Array{Float64, 2})
     dim = size(parentsimplex, 2)
     # Random linear combination coefficients
