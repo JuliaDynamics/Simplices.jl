fabc664edba439ee306cb444e0f3ee3129f06d1a
kahaaga <kahaaga@gmail.com>
2018-03-28 04:12:13 +0200

    Remove precompile stuff

--- src/Simplices.jl
+++ src/Simplices.jl
@@ -62,28 +62,28 @@ export BarycentricCoordinates,
         VolumeComputation
 
 
-function speed_test_nontrivial(dim,N)
-    for i = 1:N
-        S1,S2 = nontrivially_intersecting_simplices(dim)
-            simplexintersection(S1.', S2.')
-    end
-end
-
-
-function speed_test_sharing(dim,N)
-    for i = 1:N
-        S1,S2 = simplices_sharing_vertices(dim)
-            simplexintersection(S1.', S2.')
-    end
-end
-
-speed_test_nontrivial(3, 1)
-speed_test_nontrivial(4, 1)
-speed_test_nontrivial(5, 1)
-
-speed_test_sharing(3, 1)
-speed_test_sharing(4, 1)
-speed_test_sharing(5, 1)
+# function speed_test_nontrivial(dim,N)
+#     for i = 1:N
+#         S1,S2 = nontrivially_intersecting_simplices(dim)
+#             simplexintersection(S1.', S2.')
+#     end
+# end
+#
+#
+# function speed_test_sharing(dim,N)
+#     for i = 1:N
+#         S1,S2 = simplices_sharing_vertices(dim)
+#             simplexintersection(S1.', S2.')
+#     end
+# end
+#
+# speed_test_nontrivial(3, 1)
+# speed_test_nontrivial(4, 1)
+# speed_test_nontrivial(5, 1)
+#
+# speed_test_sharing(3, 1)
+# speed_test_sharing(4, 1)
+# speed_test_sharing(5, 1)
         # # Simplex splitting stuff
         # centroids_radii2,
         # embed,
