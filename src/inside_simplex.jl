bae9437683ede61354add8f1763988f8f3e5746c
kahaaga <kahaaga@gmail.com>
2018-03-28 03:26:11 +0200

    Fixed docs. Added `childpoint` function that does not take `dim` as input.

--- src/inside_simplex.jl
+++ src/inside_simplex.jl
@@ -1,26 +1,34 @@
 """
-    childsimplex(parentsimplex::Array{Float64, 2})
+    childpoint(parentsimplex::AbstractArray{Float64, 2})
 
- Generates a random simplex which is entirely contained within `parentsimplex`, which is a (dim+1)-by-dim array.
+Generates a random simplex which is entirely contained within `parentsimplex`,
+which is a (dim+1)-by-dim array.
 """
-function childsimplex(parentsimplex::Array{Float64, 2}, dim::Int)
-    # Convex expansion coefficients of the random simplex
+function childpoint(parentsimplex::AbstractArray{Float64, 2})
     dim = size(parentsimplex, 2)
-    rs = rand(dim + 1, dim + 1)
-    normalised_colsums = 1 ./ sum(rs, 2)
-    (normalised_colsums .* rs) * parentsimplex
+    # Random linear combination coefficients
+    R = rand(Float64, 1, dim + 1)
+
+    # Normalise the coefficients so that they sum to one. We can then create
+	# the new point as a convex linear combination of the vertices of the parent
+	# simplex.
+    R = (1 ./ sum(R, 2)) .* R
+
+    R * parentsimplex
 end
 
 """
-Create a point that lies inside `parent_simplex`
+Create a point that lies inside `parent_simplex`, which is a (dim+1)-by-dim
+array.
 """
-function childpoint(parent_simplex::Array{Float64, 2})
+function childpoint(parent_simplex::AbstractArray{Float64, 2})
     dim = size(parent_simplex, 2)
     # Random linear combination coefficients
     R = rand(1, dim + 1)
 
-    # Normalise the coefficients so that they sum to one. We can then create the new point
-    # as a convex linear combination of the vertices of the parent simplex.
+    # Normalise the coefficients so that they sum to one. We can then create
+    # the new point as a convex linear combination of the vertices of the
+    # parent simplex.
     normalised_coeffs = (1 ./ sum(R, 2)) .* R
 
     normalised_coeffs * parentsimplex
@@ -28,14 +36,17 @@ end
 
 
 """
-Create multiple (default = 100) points that lies inside `parent_simplex`.
+Create `n` (default: n = 100) points that lies inside `parent_simplex`,
+which is a (dim+1)-by-dim array.
 """
-function childpoints(parent_simplex::Array{Float64, 2}, n::Int)
+function childpoints(parent_simplex::AbstractArray{Float64, 2}, n::Int)
     dim = size(parent_simplex, 2)
-    # Random linear combination coefficients
-    R = rand(n, dim + 1)
 
-    # Normalise the coefficients so that they sum to one. We can then create the new point
+    # Random linear combination coefficients
+    R = rand(Float64, n, dim + 1)
+
+    # Normalise the coefficients so that they sum to one. We can then create
+    # the new point
     # as a convex linear combination of the vertices of the parent simplex.
     normalised_coeffs = (1 ./ sum(R, 2)) .* R
 
@@ -44,12 +55,13 @@ end
 
 
 """
-Create a point that lies inside `parent_simplex`
+Create a point that lies inside `parent_simplex`, which is a (dim+1)-by-dim
+array.
 """
-function outsidepoint(parentsimplex::Array{Float64, 2})
+function outsidepoint(parentsimplex::AbstractArray{Float64, 2})
     dim = size(parentsimplex, 2)
     # Random linear combination coefficients
-    R = rand(1, dim + 1)
+    R = rand(Float64, 1, dim + 1)
 
     # Normalise the coefficients so that they sum to one. We can then create the new point
     # as a convex linear combination of the vertices of the parent simplex.
