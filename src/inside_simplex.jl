4fe2d2c8b482d6639aa2bf5dec89dc56ca6dfcf5
kahaaga <kahaaga@gmail.com>
2018-03-28 03:55:39 +0200

    Remove duplicate function definition.

--- src/inside_simplex.jl
+++ src/inside_simplex.jl
@@ -35,23 +35,6 @@ function childpoint(parentsimplex::AbstractArray{Float64, 2}, dim::Int)
     R * parentsimplex
 end
 
-"""
-Create a point that lies inside `parent_simplex`, which is a (dim+1)-by-dim
-array.
-"""
-function childpoint(parent_simplex::AbstractArray{Float64, 2})
-    dim = size(parent_simplex, 2)
-    # Random linear combination coefficients
-    R = rand(1, dim + 1)
-
-    # Normalise the coefficients so that they sum to one. We can then create
-    # the new point as a convex linear combination of the vertices of the
-    # parent simplex.
-    normalised_coeffs = (1 ./ sum(R, 2)) .* R
-
-    normalised_coeffs * parentsimplex
-end
-
 
 """
 Create `n` (default: n = 100) points that lies inside `parent_simplex`,
