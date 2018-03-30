3a5c1c98989bb49df169c680e229ed67d2ecb80c
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-03-31 00:20:15 +0200

    Generate heaviside functions

--- src/heaviside.jl
+++ src/heaviside.jl
@@ -1,97 +1,60 @@
-heaviside(x::AbstractFloat) = ifelse(x <= 0, zero(x), ifelse(x > 0, one(x), oftype(x,0)))
-heaviside(x::Int64) = ifelse(x <= 0, zero(x), ifelse(x > 0, one(x), oftype(x,0)))
-heaviside(x::Float64) = ifelse(x <= 0, zero(x), ifelse(x > 0, one(x), oftype(x,0)))
+heaviside{T<:Number}(x::T) = ifelse(x <= 0, zero(x), ifelse(x > 0, one(x), oftype(x,0)))
+heaviside0{T<:Number}(x::T) = ifelse(x < 0, zero(x), ifelse(x >= 0, one(x), oftype(x, 0)))
 
+func_name1 = Symbol(string("heaviside"))
 
-heaviside0(x::AbstractFloat) = ifelse(x < 0, zero(x), ifelse(x >= 0, one(x), oftype(x, 0)))
-heaviside0(x::Int64) = ifelse(x < 0, zero(x), ifelse(x >= 0, one(x), oftype(x, 0)))
-heaviside0(x::Float64) = ifelse(x < 0, zero(x), ifelse(x >= 0, one(x), oftype(x, 0)))
+# generate heaviside functions for variably sized arrays
+for t in 1:5
+    @eval function $(func_name1){T<:Number}(v::AbstractArray{T, $t})
+        h = similar(v)
+        for i in eachindex(v)
+            h[i] = heaviside(v[i])
+        end
+        return(h)
+    end
+end
 
-function heaviside0(v::Array{Float64})
-    m, n = size(v)
-    h = zeros(m, n)
-
-    for i in 1:length(v)
-      h[i] = heaviside0(v[i])
+@eval function $(func_name1){T<:Number}(v::Vector{T})
+    h = similar(v)
+    for i in eachindex(v)
+        h[i] = heaviside(v[i])
     end
     return(h)
 end
 
-
-function heaviside0(v::Array{Int64})
-    m, n = size(v)
-    h = zeros(m, n)
-
-    for i in 1:length(v)
-      h[i] = heaviside0(v[i])
+@eval function $(func_name1){T<:Number}(v::RowVector{T})
+    h = similar(v)
+    for i in eachindex(v)
+        h[i] = heaviside(v[i])
     end
     return(h)
 end
 
-function heaviside0(v::Vector{Float64})
-    h = zeros(length(v), 1)
+func_name2 = Symbol(string("heaviside0"))
 
-    for i in 1:length(v)
-      h[i] = heaviside0(v[i])
+# generate heaviside0 functions for variably sized arrays
+for t in 1:5
+    @eval function $(func_name2){T<:Number}(v::AbstractArray{T, $t})
+        h = similar(v)
+        for i in eachindex(v)
+            h[i] = heaviside0(v[i])
+        end
+        return(h)
+    end
+end
+
+@eval function $(func_name2){T<:Number}(v::Vector{T})
+    h = similar(v)
+    for i in eachindex(v)
+        h[i] = heaviside0(v[i])
     end
     return(h)
 end
 
-function heaviside0(v::Vector{Int64})
-    h = zeros(length(v), 1)
-
-    for i in 1:length(v)
-      h[i] = heaviside0(v[i])
-    end
-    return(h)
-end
-
-function heaviside(v::Vector{Float64})
-    h = zeros(length(v), 1)
-
-    for i in 1:length(v)
-      h[i] = heaviside(v[i])
-    end
-    return(h)
-end
-
-function heaviside(v::Array{Float64})
-    m, n = size(v)
-    h = zeros(m, n)
-
-    for i in 1:length(v)
-      h[i] = heaviside(v[i])
-    end
-    return(h)
-end
-
-function heaviside(v::Array{Int64})
-    m, n = size(v)
-    h = zeros(m, n)
-
-    for i in 1:length(v)
-      h[i] = heaviside(v[i])
-    end
-    return(h)
-end
-
-function heaviside(v::Matrix{Int64})
-    m, n = size(v)
-    h = zeros(m, n)
-
-    for i in 1:length(v)
-      h[i] = heaviside(v[i])
-    end
-    return(h)
-end
-
-
-function heaviside(v::Matrix{Float64})
-    m, n = size(v)
-    h = zeros(m, n)
-
-    for i in 1:length(v)
-      h[i] = heaviside(v[i])
+@eval function $(func_name2){T<:Number}(v::RowVector{T})
+    h = similar(v)
+    for i in eachindex(v)
+        h[i] = heaviside0(v[i])
     end
     return(h)
 end
