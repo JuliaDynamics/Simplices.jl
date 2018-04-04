37832a286e4d1e7255b69424ece0b8cad7ec1f53
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-04-04 03:18:10 +0200

    Install SimplexSplitting in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,13 @@
 using Simplices
 using Base.Test
 
+installed = Pkg.installed()
+if !("SimplexSplitting" in keys(installed))
+    Pkg.clone("https://github.com/kahaaga/SimplexSplitting.jl")
+else
+    using SimplexSplitting
+end
+
 include("even_sampling.jl")
 include("test_delaunay.jl")
 include("test_intersectionvol.jl")
