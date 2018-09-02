8e4037b728bd6113b611b01813816eb7bf2d37b9
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-09-02 18:56:37 +0200

    Add delaunay triangulation that returns vector of index vectors.

--- src/Delaunay.jl
+++ src/Delaunay.jl
@@ -1,6 +1,7 @@
 __precompile__()
 module Delaunay
 
+using StaticArrays
 using PyCall
 const scipyspatial = PyNULL()
 
@@ -8,7 +9,56 @@ function __init__()
     copy!(scipyspatial, pyimport_conda("scipy.spatial", "scipy"))
 end
 
-function delaunayn(points::Array{T, 2}) where {T<:Number}
+function simplexindices_static(delaunaytriang::PyCall.PyObject)
+    # The indices of simplices from the pycall object
+    py_simplexinds = delaunaytriang["simplices"]
+
+    # Get the indices of the first simplex to determine
+    # the number of dimensions.
+    n_vertices = length(get(py_simplexinds, PyVector{Int32}, 0))
+
+    # Convert to Julian, accounding for index differences
+    # between Python and Julia
+    n_simplices = length(py_simplexinds)
+    juliainds = Vector{SVector{n_vertices, Int32}}(n_simplices)
+    for i = 1:n_simplices
+        v = get(py_simplexinds, PyVector{Int32}, i - 1) .+ 1
+        juliainds[i] = SVector{n_vertices}(v)
+    end
+    juliainds
+end
+
+
+function simplexindices(delaunaytriang::PyCall.PyObject)
+    # The indices of simplices from the pycall object
+    py_simplexinds = delaunaytriang["simplices"]
+
+    # Get the indices of the first simplex to determine
+    # the number of dimensions.
+    n_vertices = length(get(py_simplexinds, PyVector{Int32}, 0))
+
+    # Convert to Julian, accounding for index differences
+    # between Python and Julia
+    n_simplices = length(py_simplexinds)
+    juliainds = Vector{Vector{Int32}}(n_simplices)
+    for i = 1:n_simplices
+        v = get(py_simplexinds, PyVector{Int32}, i - 1) .+ 1
+        juliainds[i] = v
+    end
+    juliainds
+end
+
+function delaunay_static(points)
+    triang = scipyspatial[:Delaunay](points)
+    simplexindices_static(triang)
+end
+
+function delaunay(points)
+    triang = scipyspatial[:Delaunay](points)
+    simplexindices(triang)
+end
+
+function delaunayn(points)
     py = scipyspatial[:Delaunay](points)
     indices = Array{Int64, 2}(length(py["simplices"]), size(points, 2) + 1)
     pyarray_to_array!(py["simplices"], indices, Int)
@@ -21,6 +71,6 @@ function pyarray_to_array!(pyobject, arr, T)
     end
 end
 
-export delaunayn
+export delaunay, delaunay_static, delaunayn
 
 end
