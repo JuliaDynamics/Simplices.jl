6e729aa501e279177f21d5f22e3bf9eda7e8c2bf
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-11-07 10:09:19 +0100

    Still trying to get travis build to work

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,9 +5,11 @@ using Test
 using LinearAlgebra
 using Printf
 
-
+println("Running tests for Simplices.jl ....")
 
 include("even_sampling.jl")
 include("test_delaunay.jl")
 include("test_intersectionvol.jl")
 include("tensordecomposition.jl")
+
+println("Tests finished ....")
