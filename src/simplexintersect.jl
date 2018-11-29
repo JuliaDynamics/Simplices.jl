3c7c294bf5f725adaed9f1f05b77309e5f66ab90
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-11-29 15:56:27 +0100

    Removed unneccesary comments.

--- src/simplexintersect.jl
+++ src/simplexintersect.jl
@@ -34,7 +34,8 @@ function simplexintersection(S1, S2; tol::Float64 = 1/10^10)
   orientation_S2 = det([ones(1, n + 1); S2])
 
   if abs(orientation_S1) < tol || abs(orientation_S2) < tol
-    return 0
+
+    return 0.0
   end
 
   # Set volume to zero initially. Change only if there is intersection
@@ -59,7 +60,6 @@ function simplexintersection(S1, S2; tol::Float64 = 1/10^10)
     #println("")
 
     if vertices1InCircum2 + vertices2InCircum1 >= 1
-      #print("Finding barycentric coordinates\t")
       βs1in2, βs2in1, ordered_vertices1, ordered_vertices2, numof1in2, numof2in1 =
         BarycentricCoordinates(S1,S2,orientation_S1,orientation_S2,tol)
       # Trivial intersections
@@ -69,6 +69,7 @@ function simplexintersection(S1, S2; tol::Float64 = 1/10^10)
       if IsSomeContained == 2.0 # The simplices coincide
         IntVol = abs(orientation_S1)
       elseif IsSomeContained == 1.0 # One simplex is contained in the other
+
         if TriviallyContained[1] == 1.0 # Simplex1 is contained in Simplex2
           IntVol = abs(orientation_S1)
         else # Simplex2 is contained in Simplex1
@@ -82,25 +83,12 @@ function simplexintersection(S1, S2; tol::Float64 = 1/10^10)
 
         # Is there any shared face?
         if Ncomm == n
-          #print("The simplices share a face\t")
           IntVol = SharedFaceVolume(S2, βs1in2, ordered_vertices1, ordered_vertices2)
         else # The simplices do not share a face.
-          #println("\nThe simplices do not share a face")
-          #print("Intersection of boundaries\t")
-          #@time IntVert, ConvexExpIntVert  = IntersectionOfBoundaries(S1,S2,βs1in2,βs2in1, ordered_vertices1, ordered_vertices2, numof1in2, numof2in1, Ncomm, tolerance)
           IntVert, ConvexExpIntVert = IntersectionOfBoundaries_NoStorage(S1,S2,βs1in2,βs2in1, ordered_vertices1, ordered_vertices2, numof1in2, numof2in1, Ncomm, tol)
-          #@show IntVert, ConvexExpIntVert
-          #@show βs1in2,βs2in1
-          #@show ordered_vertices1,ordered_vertices2
-          #@show numof1in2,numof2in1
-          #@show Ncomm
           if !isempty(IntVert)
-            #print("PolytopeGeneratingVertices\t")
-
             IntVert,ConvexExpIntVert = PolytopeGeneratingVertices(S1,S2,IntVert,ConvexExpIntVert,βs1in2,βs2in1,ordered_vertices1,ordered_vertices2,numof1in2,numof2in1,Ncomm);
-            #print("Volume computation\t\t")
             IntVol = VolumeComputation(IntVert, ConvexExpIntVert)
-            #println("")
           end
         end
       end
@@ -112,83 +100,6 @@ function simplexintersection(S1, S2; tol::Float64 = 1/10^10)
   return IntVol
 end
 
-
-function intersectingvertices(S1::AbstractArray{Float64, 2}, S2::AbstractArray{Float64, 2}; tolerance::Float64 = 1/10^10, what = "volume")
-
-  # Dimension
-  n = size(S1, 1)
-
-  # Centroid and radii
-  c1 = Circumsphere(S1)[2:n+1]
-  c2 = Circumsphere(S2)[2:n+1]
-  r1 = Circumsphere(S1)[1]
-  r2 = Circumsphere(S2)[1]
-
-  # Orientation of simplices
-  orientation_S1 = det([ones(1, n + 1); S1])
-  orientation_S2 = det([ones(1, n + 1); S2])
-
-  if abs(orientation_S1) < tolerance || abs(orientation_S2) < tolerance
-    return Float64[]
-  end
-
-  # If the (distance between centroids)^2-(sum of radii)^2 < 0,
-  # then the simplices intersect in some way.
-  dist_difference::Float64 = (transpose(c1 - c2) * (c1 - c2) - (r1 + r2)^2)[1]
-
-  if dist_difference < 0
-    # Find the number of points of each simplex contained within the
-    # circumsphere of the other simplex
-
-    vertices1InCircum2 = SomeVertexInCircumsphere(S1, r2, c2)
-    vertices2InCircum1 = SomeVertexInCircumsphere(S2, r1, c1)
-
-    # At least one circumsphere contains vertices of the other simplex
-    #println("")
-
-    if vertices1InCircum2 + vertices2InCircum1 >= 1
-      #print("Finding barycentric coordinates\t")
-      βs1in2, βs2in1, ordered_vertices1, ordered_vertices2, numof1in2, numof2in1 = BarycentricCoordinates(S1,S2,orientation_S1,orientation_S2,tolerance)
-
-      # Trivial intersections
-      TriviallyContained = heaviside0([numof1in2 numof2in1] - (n+1))
-      IsSomeContained = sum(TriviallyContained, dims=2)[1]
-
-      if IsSomeContained == 2.0 # The simplices coincide
-        println("# The simplices coincide")
-        return copy(transpose(S1))
-      elseif IsSomeContained == 1.0 # One simplex is contained in the other
-        if TriviallyContained[1] == 1.0 # Simplex1 is contained in Simplex2
-          println("Simplex1 is contained in Simplex2")
-          return copy(transpose(S1))
-        else # Simplex2 is contained in Simplex1
-          println("Simplex2 is contained in Simplex1")
-          return copy(transpose(S2))
-        end
-      else # No simplex contains the other
-        Ncomm, ordered_vertices1, ordered_vertices2 = SharedVertices(βs1in2,ordered_vertices1,ordered_vertices2,numof1in2,numof2in1)
-
-        if Ncomm == n # The simplices share a face
-          println("The simplices share a face")
-          return SharedFaceVertices(S2, βs1in2, ordered_vertices1, ordered_vertices2)
-        else  # The simplices do not share a face.
-          IntVert, ConvexExpIntVert = IntersectionOfBoundaries_NoStorage(S1,S2,βs1in2,βs2in1, ordered_vertices1, ordered_vertices2, numof1in2, numof2in1, Ncomm, tolerance)
-          if !isempty(IntVert)
-            IntVert,ConvexExpIntVert = PolytopeGeneratingVertices(S1,S2,IntVert,ConvexExpIntVert,βs1in2,βs2in1,ordered_vertices1,ordered_vertices2,numof1in2,numof2in1,Ncomm)
-            return IntVert
-          else
-            return Float64[]
-          end
-        end
-      end
-    else # No circumsphere of either simplex contains vertices of the other simplex
-      return Float64[]
-    end
-  else
-    return Float64[]
-  end
-  return Float64[]
-end
 
 function intersection(S1::AbstractArray{Float64, 2}, S2::AbstractArray{Float64, 2},
                       c1::Vector{Float64}, c2::Vector{Float64},
@@ -237,24 +148,20 @@ function intersection(S1::AbstractArray{Float64, 2}, S2::AbstractArray{Float64, 
         end
       else # No simplex contains the other
 
-        @time Ncomm, ordered_vertices1, ordered_vertices2 = SharedVertices(βs1in2,ordered_vertices1,ordered_vertices2,numof1in2,numof2in1)
+        Ncomm, ordered_vertices1, ordered_vertices2 = SharedVertices(βs1in2,ordered_vertices1,ordered_vertices2,numof1in2,numof2in1)
 
 
         # Is there any shared face?
         if Ncomm == n
           #print("The simplices share a face\t")
-          @time IntVol = SharedFaceVolume(S2, βs1in2, ordered_vertices1, ordered_vertices2)
+          IntVol = SharedFaceVolume(S2, βs1in2, ordered_vertices1, ordered_vertices2)
         else # The simplices do not share a face.
           #println("\nThe simplices do not share a face")
           #print("Intersection of boundaries\t")
-          @time IntVert, ConvexExpIntVert  = IntersectionOfBoundaries(S1,S2,βs1in2,βs2in1, ordered_vertices1, ordered_vertices2, numof1in2, numof2in1, Ncomm, tolerance)
+          IntVert, ConvexExpIntVert  = IntersectionOfBoundaries(S1,S2,βs1in2,βs2in1, ordered_vertices1, ordered_vertices2, numof1in2, numof2in1, Ncomm, tolerance)
           if !isempty(IntVert)
-            print("PolytopeGeneratingVertices\t")
-
-            @time IntVert,ConvexExpIntVert = PolytopeGeneratingVertices(S1,S2,IntVert,ConvexExpIntVert,βs1in2,βs2in1,ordered_vertices1,ordered_vertices2,numof1in2,numof2in1,Ncomm);
-            print("Volume computation\t\t")
+            IntVert,ConvexExpIntVert = PolytopeGeneratingVertices(S1,S2,IntVert,ConvexExpIntVert,βs1in2,βs2in1,ordered_vertices1,ordered_vertices2,numof1in2,numof2in1,Ncomm);
             IntVol = VolumeComputation(IntVert, ConvexExpIntVert)
-            println("")
           end
         end
       end
